526ac8aaf7f7f58f80221dedd25152cdea775876
John Johnson <jsofteng@gmail.com>
2025-04-27 18:05:05 -0400

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          src="/mowmatters-logo.png"
+          alt="MowMatters Logo"
+          width={350}
+          height={302}
+          priority
+        />
+        <h1>Coming Soon!</h1>
+      </main>
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
